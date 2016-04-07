33f96bb997bd767151553e3d826c9098c9e9a305
Aziz Boudi <azicode4@gmail.com>
2016-04-07 12:31:26 -0700

    - Initial commit for the Collapsible Date Picker

---
+++ Collapsible-DatePicker/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Collapsible-DatePicker
+//
+//  Created by aziz omar boudi  on 4/7/16.
+//  Copyright © 2016 jogabo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+  @IBOutlet weak var tableTest: UITableView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("datePickerCell", forIndexPath: indexPath)
+
+    return cell
+  }
+
+  
+
+
+}
+
